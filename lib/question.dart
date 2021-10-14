7522fc00b39d40f2b87aeacdc9f44ad08173b931
Roderik Schiele <roderik@esbl.digital>
2021-10-14 15:44:06 +0200

    progress

---
+++ lib/question.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  Question(this.questionText);
+
+  // Question(text){
+
+  //   this.questionText = text;
+  // }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(15),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
